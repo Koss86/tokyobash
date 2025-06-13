a4523e8d2498b6a81b513b06ac9d213ae335e002
Koss86 <jacobdelarosa123@gmail.com>
2025-06-13 06:55:24 -0500

    Themes (#3)
    * testing out adding themes
    
    * started to add catppuccin
    
    * It's coming together.
    
    * Changed arrays to pointers.
    
    Changed color_1,2..etc from char arrays to char *. I feel assigning a
    pointer the address of the color would be more efficient, instead of all of the strncpy's I was
    using before.
    
    Added catppuccin and tokyonight.
    
    * Added kanagawa theme
    
    * Theme functionality pretty much done.
    
    Made the argc statements more robust. Now will behave as expected
    whether any args are passed or not and in any order. If no args are
    passed it will default to tokyonight theme with no blank line spacer.

--- tokyobash.c
+++ tokyobash.c
@@ -5,14 +5,15 @@
 #include <string.h>
 #include <unistd.h>
 
+#define BUF_SIZE 25
 #define ABV_PATH_LEN1 24
 #define ABV_PATH_LEN2 23
 #define ABV_PATH_LEN_T 50 // 24 + 23 + "..."
 
-bool in_home(char *path, char *home, int Hleng);
-void replace_home(char *path, char *home, int leng, int indx);
-void abrv_path(char *path, int leng);
-void rem_curDir(char *path, int leng);
+bool in_home(char *path, char *home, int Hlen);
+void replace_home(char *path, char *home, int Plen, int indx);
+void abrv_path(char *path, int Plen);
+void rem_curDir(char *path, int Plen);
 bool in_mnt(char *path);
 
 int main(int argc, char **argv) {
@@ -20,11 +21,96 @@ int main(int argc, char **argv) {
   char path[PATH_MAX];
   // Check to make sure we got the path
   if (!getcwd(path, sizeof(path))) {
-    perror("Error: getcwd() failed to retrieve path\n");
+    perror("Error: getcwd() failed to retrieve path\\n");
     exit(1);
   }
 
-  int leng = strlen(path);
+  char reset[] = "\\[\\e[00m\\]";
+  char bold[] = "\\[\\e[1m\\]";
+  char cyan[] = "\\[\\e[38;5;86m\\]";
+  char dCyan[] = "\\[\\e[38;2;104;155;196m\\]";
+  char blue[] = "\\[\\e[38;5;4m\\]";
+  char lBlue[] = "\\[\\e[38;5;117m\\]";
+  char red[] = "\\[\\e[38;2;255;77;77m\\]";
+  char lRed[] = "\\[\\e[38;5;211m\\]";
+  char peach[] = "\\[\\e[38;5;223m\\]";
+  char purple[] = "\\[\\e[38;5;182m\\]";
+  char pink[] = "\\[\\e[38;5;217m\\]";
+  char orange[] = "\\[\\e[38;5;214m\\]";
+  char khaki[] = "\\[\\e[38;2;238;232;170m\\]";
+  char lime[] = "\\[\\e[38;5;149m\\]";
+
+  char *color_usr = &cyan[0];
+  char *color_time = &lBlue[0];
+  char *color_path = &blue[0];
+  char *color_mnt = &peach[0];
+  char *color_root = &lRed[0];
+
+  if (argc > 1) {
+
+    if (argv[1][0] >= 'a' && argv[1][0] <= 'z') {
+
+      if (!strcmp(argv[1], "tokyonight")) {
+        color_usr = &cyan[0];
+        color_time = &lBlue[0];
+        color_path = &blue[0];
+        color_mnt = &peach[0];
+        color_root = &lRed[0];
+
+      } else if (!strcmp(argv[1], "catppuccin")) {
+        color_usr = &peach[0];
+        color_time = &pink[0];
+        color_path = &purple[0];
+        color_mnt = &lBlue[0];
+        color_root = &orange[0];
+
+      } else if (!strcmp(argv[1], "kanagawa")) {
+        color_usr = &red[0];
+        color_time = &dCyan[0];
+        color_path = &khaki[0];
+        color_mnt = &lime[0];
+        color_root = &purple[0];
+      }
+
+      if (argc > 2) {
+        if (atoi(argv[2]) > 1) {
+          printf("\\n");
+        }
+      }
+
+    } else {
+
+      if (atoi(argv[1]) > 1) {
+        printf("\\n");
+      }
+
+      if (argc > 2) {
+        if (!strcmp(argv[2], "tokyonight")) {
+          color_usr = &cyan[0];
+          color_time = &lBlue[0];
+          color_path = &blue[0];
+          color_mnt = &peach[0];
+          color_root = &lRed[0];
+
+        } else if (!strcmp(argv[2], "catppuccin")) {
+          color_usr = &peach[0];
+          color_time = &pink[0];
+          color_path = &purple[0];
+          color_mnt = &lBlue[0];
+          color_root = &orange[0];
+
+        } else if (!strcmp(argv[2], "kanagawa")) {
+          color_usr = &red[0];
+          color_time = &dCyan[0];
+          color_path = &khaki[0];
+          color_mnt = &lime[0];
+          color_root = &purple[0];
+        }
+      }
+    }
+  }
+
+  int Plen = strlen(path);
   char *pHome = getenv("HOME");
   bool pHomeState = true;
 
@@ -32,78 +118,60 @@ int main(int argc, char **argv) {
   if (!pHome) {
     pHomeState = false;
   } else {
-    int Hleng = strlen(pHome);
+    int Hlen = strlen(pHome);
     // If path contains $HOME replace it with '~'
-    if (in_home(path, pHome, Hleng)) {
-      replace_home(path, pHome, leng, Hleng);
-      leng = (leng - Hleng) + 1;
+    if (in_home(path, pHome, Hlen)) {
+      replace_home(path, pHome, Plen, Hlen);
+      Plen = (Plen - Hlen) + 1;
     }
   }
 
-  if (leng > ABV_PATH_LEN_T) {
-    abrv_path(path, leng);
-    leng = ABV_PATH_LEN_T;
+
+  if (Plen > ABV_PATH_LEN_T) {
+    abrv_path(path, Plen);
+    Plen = ABV_PATH_LEN_T;
   }
 
-  // Add or remove color & format codes here
-  // to use in the printfs below
-  char bold[] = "\\[\\e[1m\\]";
-  char reset[] = "\\[\\e[00m\\]";
-  char color_1[] = "\\[\\e[38;5;86m\\]";
-  char color_2[] = "\\[\\e[38;5;4m\\]";
-  char color_3[] = "\\[\\e[38;5;117m\\]";
-  char color_4[] = "\\[\\e[38;5;222m\\]";
-  char color_5[] = "\\[\\e[38;5;211m\\]";
-
-  // This checks if the term just opened,
-  // W/o this, when the term first opens there
-  // would be a blank line above the prompt.
-  if (argc > 1) {
-    if (atoi(argv[1]) > 1) {
-      printf("\\n");
-    }
-  }
-
-  // If getenv() returned NULL just print standard prompt
+  // If getenv() returned NULL, just print standard prompt
   if (!pHomeState) {
-    rem_curDir(path, leng);
-    printf("%s%s\\u@\\h%s:%s [\\t] %s%s\\W/\\n", bold, color_1, reset, color_2,
-           path, bold);
+    rem_curDir(path, Plen);
+    printf("%s%s\\u@\\h%s:%s [\\t] %s%s%s\\W/\\n", bold, color_usr, reset,
+           color_time, color_path, path, bold);
   } else {
     if (path[0] == '~') {
       // Removing current directoy from path to change
       // text to bold before adding it back with \\W.
-      rem_curDir(path, leng);
-      if (leng > 1) {
-        printf("%s%s\\u@\\h%s:%s [\\t] %s%s\\W/\\n", bold, color_1, reset,
-               color_2, path, bold);
+      rem_curDir(path, Plen);
+      if (Plen > 1) {
+        printf("%s%s\\u@\\h%s:%s [\\t] %s%s%s\\W/\\n", bold, color_usr, reset,
+               color_time, color_path, path, bold);
       } else {
-        printf("%s%s\\u@\\h%s:%s [\\t] %s\\W/\\n", bold, color_1, reset,
-               color_2, bold);
+        printf("%s%s\\u@\\h%s:%s [\\t] %s%s\\W/\\n", bold, color_usr, reset,
+               color_time, color_path, bold);
       }
     } else {
 
       bool inMnt = in_mnt(path);
-      rem_curDir(path, leng);
+      rem_curDir(path, Plen);
 
       if (inMnt) {
-        printf("%s%s\\u@\\h%s:%s [\\t] %s%s%s\\W/\\n", bold, color_1, reset,
-               color_2, color_4, path, bold);
-      } else if (leng > 1) {
-        printf("%s%s\\u@\\h%s:%s [\\t] %s%s%s\\W/\\n", bold, color_1, reset,
-               color_2, color_5, path, bold);
+        printf("%s%s\\u@\\h%s:%s [\\t] %s%s%s\\W/\\n", bold, color_usr, reset,
+               color_time, color_mnt, path, bold);
+      } else if (Plen > 1) {
+        printf("%s%s\\u@\\h%s:%s [\\t] %s%s%s\\W/\\n", bold, color_usr, reset,
+               color_time, color_root, path, bold);
       } else {
-        printf("%s%s\\u@\\h%s:%s [\\t] %s%s\\W\\n", bold, color_1, reset,
-               color_2, bold, color_5);
+        printf("%s%s\\u@\\h%s:%s [\\t] %s%s\\W\\n", bold, color_usr, reset,
+               color_time, bold, color_root);
       }
     }
   }
-  printf("  %s└:> %s%s", color_1, reset, color_3);
+  printf("  %s└:> %s", color_usr, reset);
   return 0;
 }
 
-bool in_home(char *path, char *home, int Hleng) {
-  for (int i = 0; i < Hleng; i++) {
+bool in_home(char *path, char *home, int Hlen) {
+  for (int i = 0; i < Hlen; i++) {
     if (path[i] != home[i]) {
       return false;
     }
@@ -111,33 +179,33 @@ bool in_home(char *path, char *home, int Hleng) {
   return true;
 }
 
-void replace_home(char *path, char *home, int leng, int Hleng) {
+void replace_home(char *path, char *home, int Plen, int Hlen) {
   path[0] = '~';
-  if (leng == Hleng) {
+  if (Plen == Hlen) {
     path[1] = '\0';
   } else {
-    int indx = Hleng;
-    for (int i = 1; i < leng; i++) {
+    int indx = Hlen;
+    for (int i = 1; i < Plen; i++) {
       path[i] = path[indx++];
     }
     path[indx] = '\0';
   }
 }
 
-void abrv_path(char *path, int leng) {
+void abrv_path(char *path, int Plen) {
   int i;
   for (i = ABV_PATH_LEN1; i < ABV_PATH_LEN1 + 3; i++) {
     path[i] = '.';
   }
-  int indx = leng - ABV_PATH_LEN2;
+  int indx = Plen - ABV_PATH_LEN2;
   for (i = ABV_PATH_LEN1 + 3; i < ABV_PATH_LEN_T; i++) {
     path[i] = path[indx++];
   }
   path[ABV_PATH_LEN_T] = '\0';
 }
 
-void rem_curDir(char *path, int leng) {
-  for (int i = leng - 1; i > -1; i--) {
+void rem_curDir(char *path, int Plen) {
+  for (int i = Plen - 1; i > -1; i--) {
     if (path[i] == '/') {
       path[i + 1] = '\0';
       break;
