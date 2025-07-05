e6aa5bd410272a1d44d693a4823324bb2f98f6f9
Koss86 <jacobdelarosa123@gmail.com>
2025-07-05 17:25:16 -0500

    Added some comments.
    Need to wrap this all into one statment i think, then check state if
    should return, then i wont need to repeat code.

--- src/test/fetchtimer.c
+++ src/test/fetchtimer.c
@@ -21,8 +21,8 @@ bool shouldFetch(FetchOpts *fetchSettings) {
     char cur_date[] = "2025-03-15";
     char fetch_date[] = "2025-03-15";
 
-    char cur_time[] = "10:30:43";
-    char fetch_time[] = "10:19:22";
+    char cur_time[] = "00:30:43";
+    char fetch_time[] = "00:15:22";
 
 
     if (fetch_date[2] != cur_date[2] || fetch_date[3] != cur_date[3]) return true; // Year
@@ -34,8 +34,12 @@ bool shouldFetch(FetchOpts *fetchSettings) {
     int i_cbuf;
     int diff = 0;
 
-    if (fetchSettings->state == Hour) {
+    Fetchtimer state = fetchSettings->state;
 
+    // thinking of wraping these into one statement, and check state if should return and when?
+    if (state == Hour) {
+
+        // Check if longer than 1 day since update.
         if (fetch_date[8] != cur_date[8] || fetch_date[9] != cur_date[9]) {  // Day
 
             for (int i = 0; i < 3; i++) {
@@ -54,6 +58,9 @@ bool shouldFetch(FetchOpts *fetchSettings) {
             int dayDif;
             dayDif = i_cbuf - i_fbuf;
 
+            // Dont return if dayDif == 1 because
+            // if updates are around midnight, it 
+            // could think it's been a day when its been 10mins.
             if (dayDif > 1) {
                 return true;
             }
@@ -84,7 +91,7 @@ bool shouldFetch(FetchOpts *fetchSettings) {
             }
 
             if (diff >= fetchSettings->amount) {
-                if (diff == 1) { // Check minutes if only an hour apart to make sure actually an hour apart.
+                if (diff == 1) { // Check minutes if only an hour apart to make sure its actually an hour apart.
                     for (int i = 0; i < 3; i++) {
                         if (i == 2) {
                             fbuf[i] = '\0';
@@ -98,7 +105,9 @@ bool shouldFetch(FetchOpts *fetchSettings) {
                     i_fbuf = atoi(fbuf);
                     i_cbuf = atoi(cbuf); 
 
-                    if ((60 - i_fbuf) + i_cbuf >= 60) {
+                    // This ensures it's been 60mins or longer since last update,
+                    // and that the date just didnt change making it look like a day since update.
+                    if (((60 - i_fbuf) + i_cbuf) >= 60) {
                         return true;
                     }
                 } else {
